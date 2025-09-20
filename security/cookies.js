ebff7a265ee15da116307cbcf0b6ddd0d76479e3
Oleg <luzeninoleg2012@gmail.com>
2025-09-20 14:54:32 +0300

    cookie defense practice

---
+++ security/cookies.js
@@ -0,0 +1,5 @@
+// httpOnly in headers
+// Secure in headers with process.env.NODE_ENV  = 'production' => only sent over https
+// SameSite in headers => only sent in the same domain
+// Domain in headers => only sent in the same domain
+// Path in headers => only sent in the same path
