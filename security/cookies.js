73dfee148a231f604fa0e7b868cbda1d1b3c6e31
Oleg <luzeninoleg2012@gmail.com>
2025-09-21 16:15:11 +0300

    security libs for RN

--- security/cookies.js
+++ security/cookies.js
@@ -3,3 +3,6 @@
 // SameSite in headers => only sent in the same domain
 // Domain in headers => only sent in the same domain
 // Path in headers => only sent in the same path
+
+// use DOMPurify to sanitize the HTML
+// Same Origin <> Same Site, Origin => protocol, domain, port, site = site + 1 subdomain
---
+++ security/react_native_libs.js
@@ -0,0 +1,2 @@
+// security libs for storage and sensitive data
+// react-native-sensitive-info / react-native-keychain
