511b4656ad28abd0bf07414dc9fdffa1c5d38cb3
Oleg <luzeninoleg2012@gmail.com>
2025-09-22 13:56:17 +0300

    keyboard hints

--- react_native_tips/tips.js
+++ react_native_tips/tips.js
@@ -18,3 +18,45 @@ export function PlantlyImage() {
 // Navigation
 // for Link if you render a component you need to pass asChild prop to render the component as a child of the Link
 // to make a full screen modal you need to place the modal to the same level to render it on top of the tabs
+
+<Stack.Screen
+  name="new"
+  options={{
+    presentation: "modal",
+    title: "New Plant",
+    animation: "fade",
+  }}
+/>;
+
+// Keyboard
+
+// npx expo install react-native-keyboard-aware-scroll-view
+// Use this not to let the keyboard cover the input fields
+
+<KeyboardAwareScrollView
+  style={styles.container}
+  contentContainerStyle={styles.contentContainer}
+  keyboardShouldPersistTaps="handled"
+>
+  <View style={styles.centered}>
+    <PlantlyImage />
+  </View>
+  <Text style={styles.label}> Name</Text>
+  <TextInput
+    style={styles.input}
+    value={plant.name}
+    onChangeText={text => setPlant({ ...plant, name: text })}
+    placeholder="Add a plant"
+    autoCapitalize="words"
+  />
+  <Text style={styles.label}>Watering Frequency (every x days)</Text>
+  <TextInput
+    style={styles.input}
+    value={plant.wateringFrequency}
+    onChangeText={text => setPlant({ ...plant, wateringFrequency: text })}
+    placeholder="E.g. Every 5 days"
+    keyboardType="number-pad"
+  />
+  <PlantlyButton title="Add Plant" onPress={handleSubmitPlant} />
+</KeyboardAwareScrollView>;
+//
